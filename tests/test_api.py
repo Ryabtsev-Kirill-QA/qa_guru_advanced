a50428dd9425926e445ee9b6f0d1d878b13c9fcc
Kirill <ryabtsev231297@gmail.com>
2025-10-29 14:30:19 +0300

    add pagination api test

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,6 +2,7 @@ import pytest
 import requests
 from http import HTTPStatus
 from models.User import User
+from models.Pagination import Pagination
 
 
 @pytest.fixture
@@ -47,3 +48,50 @@ class TestUserEndpoint:
     def test_user_invalid_values(self, app_url, user_id):
         response = requests.get(f"{app_url}/api/users/{user_id}")
         assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+
+
+class TestPagination:
+    """Тесты для пагинации в эндпоинте /users"""
+
+    def test_default_pagination(self, app_url):
+        response = requests.get(f"{app_url}/api/users/")
+        users_with_pagination = response.json()
+
+        assert response.status_code == HTTPStatus.OK
+        assert len(users_with_pagination) == 5
+
+        users_pagination = response.json()
+        Pagination.model_validate(users_pagination)
+
+    @pytest.mark.parametrize("size, expected_pages", [(1, 12), (3, 4), (12, 1)])
+    def test_get_users_page_calculation(self, app_url, size, expected_pages):
+        response = requests.get(f"{app_url}/api/users/?size={size}")
+
+        assert response.status_code == HTTPStatus.OK
+        assert response.json()["pages"] == expected_pages
+
+    @pytest.mark.parametrize("page, size, expected_count", [
+        (1, 5, 5),
+        (2, 5, 5),
+        (3, 5, 2),
+        (1, 12, 12),
+        (2, 12, 0)
+    ])
+    def test_get_users_different_pages_pagination(self, app_url, page, size, expected_count):
+        response = requests.get(f"{app_url}/api/users/?page={page}&size={size}")
+
+        assert response.status_code == HTTPStatus.OK
+        assert response.json()["page"] == page
+        assert response.json()["size"] == size
+        assert len(response.json()["items"]) == expected_count
+
+    def test_get_users_different_data_in_pages(self, app_url):
+        response_page1 = requests.get(f"{app_url}/api/users/?page=1&size=6")
+        response_page2 = requests.get(f"{app_url}/api/users/?page=2&size=6")
+
+        data_page1 = response_page1.json()
+        data_page2 = response_page2.json()
+
+        assert data_page1["items"] != data_page2["items"]
+        assert data_page1["page"] == 1
+        assert data_page2["page"] == 2
